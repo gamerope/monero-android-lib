631d588012183a39f9f81de6ba00db1e1d6e9e0b
gamerope <83300383+gamerope@users.noreply.github.com>
2022-01-27 16:12:09 +0100

    Update boost-fetch.sh

--- external-libs/script/boost-fetch.sh
+++ external-libs/script/boost-fetch.sh
@@ -6,11 +6,11 @@ source script/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version=1_58_0
-dot_version=1.58.0
+version=1_63_0
+dot_version=1.63.0
 
 if [ ! -f "boost_${version}.tar.gz" ]; then
-  wget https://downloads.sourceforge.net/project/boost/boost/${dot_version}/boost_${version}.tar.gz
+  wget https://boostorg.jfrog.io/artifactory/main/release/${dot_version}/source/boost_${version}.tar.gz
 fi
 
 if [ ! -d "boost_${version}" ]; then
